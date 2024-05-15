b5617aacd72cb621890e0b7e7369b31b40d01d32
Bykamri <mafrizalsuhartono@gmail.com>
2024-05-16 02:17:09 +0700

    Initilized Website

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig, squooshImageService } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    service: squooshImageService(),
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
+});
---
+++ src/utils/constant.ts
@@ -0,0 +1,20 @@
+export const socials = [
+    // {
+    //     icon: "mdi:map-marker",
+    //     href: "/",
+    //     username: "Surabaya, East Java",
+    // },
+    {
+        slot: "icon",
+        icons: "map-pin",
+        title: "Surabaya",
+    },
+    {
+        slot: "icon",
+        icons: "linkedin",
+        href: "https://www.linkedin.com/in/mafrizal-suhartono/",
+        title: "Connect on Linkedin",
+        target: "_blank",
+        as: "a"
+    }
+]
