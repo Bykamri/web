411ec2657832d7e0403386a1e362e071ac1a987d
Bykamri <mafrizalsuhartono@gmail.com>
2024-05-19 14:29:58 +0700

    changes

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,5 +1,5 @@
 export const info = {
-    baseUrl: "https://astro-portfolio-uzair.vercel.app",
+    baseUrl: "https://afrizalsuhartono.my.id",
     name: "Muhammad Afrizal Suhartono",
     image: "/favicon/android-chrome-384x384.png",
     jobDescription: "Fresh Graduate",
@@ -38,7 +38,7 @@ export const info = {
         {
             title: "Astro Portfolio",
             isFeatured: true,
-            thumbnail: "src/assets/astro-portfolio.pnhttps://github.com/Bykamri/web/blob/233f7d9400f0510b674c05e3e16982a03c431a9f/src/assets/astro-portfolio.png",
+            thumbnail: "/static/astro-portfolio.png",
             githubUrl: "https://github.com/uzzii-21/astro-portfolio",
             liveUrl: "https://astro-portfolio-uzair.vercel.app/",
             techs: [
@@ -48,7 +48,7 @@ export const info = {
         {
             title: "Astro Portfolio",
             isFeatured: false,
-            thumbnail: "src/assets/astro-portfolio.png",
+            thumbnail: "/static/astro-portfolio.png",
             githubUrl: "https://github.com/Bykamri/web/blob/233f7d9400f0510b674c05e3e16982a03c431a9f/src/assets/astro-portfolio.png",
             liveUrl: "https://astro-portfolio-uzair.vercel.app/",
             techs: [
@@ -58,7 +58,7 @@ export const info = {
         {
             title: "Astro Portfolio",
             isFeatured: false,
-            thumbnail: "src/assets/astro-portfolio.png",
+            thumbnail: "/static/astro-portfolio.png",
             githubUrl: "https://github.com/Bykamri/web/blob/233f7d9400f0510b674c05e3e16982a03c431a9f/src/assets/astro-portfolio.png",
             liveUrl: "https://astro-portfolio-uzair.vercel.app/",
             techs: [
